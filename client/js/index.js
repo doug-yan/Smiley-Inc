f9fb6dd80a97e0c62f9ee7a29f72e1a7b02ab124
Stephanie Baum <sbaum1994@gmail.com>
2015-08-03 19:58:32 -0400

    make it play when you click

--- client/js/index.js
+++ client/js/index.js
@@ -11,7 +11,30 @@ function playSong(type, title, artist) {
   instrumentalAudio.play();
 }
 
+function clearSearchResults() {
+	//clear previous results
+	this.searchResults.innerHTML = '';
+	this.searchResultsContainer.style.display = 'none';
+}
+
 function search() {
-	this.searchResultsContainer.style.display = 'block';
+	var results = [{title:'The_Kill', artist:'30_Seconds_to_Mars', type:'instrumental'}];
+	clearSearchResults(); 
+
+	//create result list
+	results.forEach(function(result) {
+		var songWrapper = document.createElement('li');
+		var song = document.createElement('span');
+		song.innerText = result.title + ' - ' + result.artist + '(' + result.type + ')';
+		songWrapper.appendChild(song);
+
+		songWrapper.addEventListener('click', function() {
+			playSong(result.type, result.title, result.artist);
+			clearSearchResults();
+		});
+
+		this.searchResults.appendChild(songWrapper);
+		this.searchResultsContainer.style.display = 'block';
+	});
 	event.preventDefault();
 }
