d278025f6e97671c5ade8e81afa03c9193966874
Stephanie Baum <sbaum1994@gmail.com>
2015-08-03 20:43:44 -0400

    Added some basic layout styling, basic search form, results

--- client/js/index.js
+++ client/js/index.js
@@ -10,3 +10,31 @@ function playSong(type, title, artist) {
   instrumentalAudio.load();
   instrumentalAudio.play();
 }
+
+function clearSearchResults() {
+  //clear previous results
+  this.searchResults.innerHTML = '';
+  this.searchResultsContainer.style.display = 'none';
+}
+
+function search() {
+  var results = [{title:'The_Kill', artist:'30_Seconds_to_Mars', type:'instrumental'}];
+  clearSearchResults(); 
+
+  //create result list
+  results.forEach(function(result) {
+    var songWrapper = document.createElement('li');
+    var song = document.createElement('span');
+    song.innerText = result.title + ' - ' + result.artist + '(' + result.type + ')';
+    songWrapper.appendChild(song);
+
+    songWrapper.addEventListener('click', function() {
+      playSong(result.type, result.title, result.artist);
+      clearSearchResults();
+    });
+
+    this.searchResults.appendChild(songWrapper);
+    this.searchResultsContainer.style.display = 'block';
+  });
+  event.preventDefault();
+}
