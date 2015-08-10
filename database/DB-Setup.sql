9e5263d97bbe69e465d6889d7840cfb56c9fd309
Tim Mendez <tmendez@rms.com>
2015-08-10 14:16:17 -0700

    User authentication via Google

--- database/DB-Setup.sql
+++ database/DB-Setup.sql
@@ -1,12 +1,7 @@
 CREATE DATABASE OriginateIdol;
 
 DROP TABLE IF EXISTS Highscores;
-DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Songs;
-
-CREATE TABLE Users (
-  userId CHAR(10) primary key
-);
 
 CREATE TABLE Songs (
   title VARCHAR(40),
@@ -18,17 +13,15 @@ CREATE TABLE Songs (
 );
 
 CREATE TABLE Highscores (
-  userId CHAR(10) NOT NULL,
+  userId CHAR(21) NOT NULL,
+  name VARCHAR(75) NOT NULL,
+  picture VARCHAR(150),
   title VARCHAR(40) NOT NULL,
   artist VARCHAR(40) NOT NULL,
   score INT,
   PRIMARY KEY (userId, title, artist),
-  FOREIGN KEY (title, artist) REFERENCES Songs(title, artist),
-  FOREIGN KEY (userId) REFERENCES Users(userId)
+  FOREIGN KEY (title, artist) REFERENCES Songs(title, artist)
 );
-
-INSERT INTO Users VALUES
-  ('1234567890'), ('0987654321'), ('1029384756'), ('5647382910'), ('9999999999');
 
 INSERT INTO Songs VALUES
   ('The_Kill', '30_Seconds_to_Mars', 6, 243, 'Alternative'),
@@ -37,11 +30,11 @@ INSERT INTO Songs VALUES
   ('Shake_it_Off', 'Taylor_Swift', 4, 241, 'Pop');
 
 INSERT INTO Highscores VALUES
-  ('1234567890', 'The_Kill', '30_Seconds_to_Mars', 839),
-  ('0987654321', 'The_Kill', '30_Seconds_to_Mars', 350),
-  ('1029384756', 'The_Kill', '30_Seconds_to_Mars', 753),
-  ('5647382910', 'The_Kill', '30_Seconds_to_Mars', 125),
-  ('1234567890', 'Lose_Yourself', 'Eminem', 359),
-  ('1234567890', 'Shake_it_Off', 'Taylor_Swift', 839),
-  ('0987654321', 'Blank_Space', 'Taylor_Swift', 525),
-  ('0987654321', 'Lose_Yourself', 'Eminem', 745);
+  ('227463955755766435220', 'Test User1', 'http://s2.postimg.org/mlc6ck4x5/originate_Interns.png', 'The_Kill', '30_Seconds_to_Mars', 839),
+  ('098765432112345679934', 'Test User2', 'http://s2.postimg.org/mlc6ck4x5/originate_Interns.png', 'The_Kill', '30_Seconds_to_Mars', 350),
+  ('102938475685018599518', 'Test User3', 'http://s2.postimg.org/mlc6ck4x5/originate_Interns.png', 'The_Kill', '30_Seconds_to_Mars', 753),
+  ('564738291058175551825', 'Test User4', 'http://s2.postimg.org/mlc6ck4x5/originate_Interns.png', 'The_Kill', '30_Seconds_to_Mars', 125),
+  ('227463955755766435220', 'Test User1', 'http://s2.postimg.org/mlc6ck4x5/originate_Interns.png', 'Lose_Yourself', 'Eminem', 359),
+  ('227463955755766435220', 'Test User1', 'http://s2.postimg.org/mlc6ck4x5/originate_Interns.png', 'Shake_it_Off', 'Taylor_Swift', 839),
+  ('098765432112345679934', 'Test User2', 'http://s2.postimg.org/mlc6ck4x5/originate_Interns.png', 'Blank_Space', 'Taylor_Swift', 525),
+  ('098765432112345679934', 'Test User2', 'http://s2.postimg.org/mlc6ck4x5/originate_Interns.png', 'Lose_Yourself', 'Eminem', 745);
