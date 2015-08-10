3a5b5538bde1b3e3cfda3adb9bba763bf46733ed
Stephanie Baum <stephanie.baum@rms.com>
2015-08-10 12:40:04 -0700

    add visualizer, refactor things

--- client/js/index.js
+++ client/js/index.js
@@ -1,4 +1,7 @@
 var instrumentalAudio = document.getElementById('instrumental');
+userRecorder = new RecordingObject();
+var micInit = false;
+
 
 /*
  * type - 'instrumental' or 'acapella'
@@ -36,8 +39,32 @@ function addSearchResults(results) {
 }
 
 
-// send search input
+function initMic() {
+  micInit = true;
+  if (!navigator.getUserMedia)
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({audio:true}, success, function(e) {
+      console.warn('Error capturing audio.');
+    });
+  }
+  else {
+    console.warn('getUserMedia not supported in this browser.');
+  }
+}
+
+function success(e) {
+  userRecorder.setupAudioStream(e);
+}
+
+
+
 $(document).ready(function() {
+  canvasContext = $("#canvas").get()[0].getContext("2d");
+  visualizer = new Visualizer(canvasContext);
+
   $('#searchForm').bind('submit', function() {
     event.preventDefault();
     clearSearchResults();
@@ -74,13 +101,16 @@ $(document).ready(function() {
   });
 
   $('#recordButton').bind('click', function() {
-    record();
+    userRecorder.startRecording();
+    if (!micInit) {
+      initMic();
+    }
   })
 
   $('#stopRecordingButton').bind('click', function() {
-    userRecording = stopRecording();
+    userRecording = userRecorder.stopRecording();
     var fd = new FormData();
-    fd.append('recording', userRecording, recording);
+    fd.append('recording', userRecording, true);
 
     $.ajax({
       type: 'POST',
