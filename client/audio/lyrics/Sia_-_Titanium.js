b90a40f3d8dbebecd60dc68faf74cfe2ab7c45ce
Tim Mendez <tmendez@rms.com>
2015-08-13 11:49:08 -0700

    titanium lyrics

---
+++ client/audio/lyrics/Sia_-_Titanium.js
@@ -0,0 +1,162 @@
+var lyrics = [
+  {
+    'words': "You shout it out,",
+    'delay':
+  },
+  {
+    'words': "But I can't hear a word you say",
+    'delay':
+  },
+  {
+    'words': "I'm talking loud, not saying much",
+    'delay':
+  },
+  {
+    'words': "I'm criticized but all your bullets ricochet",
+    'delay':
+  },
+  {
+    'words': "You shoot me down, but I get up",
+    'delay':
+  },
+  {
+    'words': "I'm bulletproof, nothing to lose",
+    'delay':
+  },
+  {
+    'words': "Fire away, fire away",
+    'delay':
+  },
+  {
+    'words': "Ricochet, you take your aim",
+    'delay':
+  },
+  {
+    'words': "Fire away, fire away",
+    'delay':
+  },
+  {
+    'words': "You shoot me down but I won't fall",
+    'delay':
+  },
+  {
+    'words': "I am titanium",
+    'delay':
+  },
+  {
+    'words': "You shoot me down but I won't fall",
+    'delay':
+  },
+  {
+    'words': "I am titanium",
+    'delay':
+  },
+  {
+    'words': "Cut me down",
+    'delay':
+  },
+  {
+    'words': "But it's you who'll have further to fall",
+    'delay':
+  },
+  {
+    'words': "Ghost town and haunted love",
+    'delay':
+  },
+  {
+    'words': "Raise your voice, sticks and stones may break my bones",
+    'delay':
+  },
+  {
+    'words': "I'm talking loud not saying much",
+    'delay':
+  },
+  {
+    'words': "I'm bulletproof, nothing to lose",
+    'delay':
+  },
+  {
+    'words': "Fire away, fire away",
+    'delay':
+  },
+  {
+    'words': "Ricochet, you take your aim",
+    'delay':
+  },
+  {
+    'words': "Fire away, fire away",
+    'delay':
+  },
+  {
+    'words': "You shoot me down but I won't fall",
+    'delay':
+  },
+  {
+    'words': "I am titanium",
+    'delay':
+  },
+  {
+    'words': "You shoot me down but I won't fall",
+    'delay':
+  },
+  {
+    'words': "I am titanium",
+    'delay':
+  },
+  {
+    'words': "I am titanium",
+    'delay':
+  },
+  {
+    'words': "I am titanium",
+    'delay':
+  },
+  {
+    'words': "Stone-heart, machine gun",
+    'delay':
+  },
+  {
+    'words': "Firing at the ones who run",
+    'delay':
+  },
+  {
+    'words': "Stone heart loves bulletproof glass",
+    'delay':
+  },
+  {
+    'words': "You shoot me down but I won't fall",
+    'delay':
+  },
+  {
+    'words': "I am titanium",
+    'delay':
+  },
+  {
+    'words': "You shoot me down but I won't fall",
+    'delay':
+  },
+  {
+    'words': "I am titanium",
+    'delay':
+  },
+  {
+    'words': "You shoot me down but I won't fall",
+    'delay':
+  },
+  {
+    'words': "I am titanium",
+    'delay':
+  },
+  {
+    'words': "You shoot me down but I won't fall",
+    'delay':
+  },
+  {
+    'words': "I am titanium",
+    'delay':
+  },
+  {
+    'words': "I am titanium",
+    'delay':
+  }
+];
