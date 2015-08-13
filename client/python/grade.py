4dae579978c45bb65e40e190f4c3e25806ab4c12
Tony Cao <tonycao96@gmail.com>
2015-08-12 18:08:52 -0700

    work

--- client/python/grade.py
+++ client/python/grade.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 
-import librosa
 import sys
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-import analyse
-import pyaudio
-import midi
 import subprocess
 import aubio
 from aubio import source, onset
@@ -37,9 +32,29 @@ def get_notes_with_onsets(song, win_s = 512, hop_s = 256, samplerate = 44100, to
   return notes
 
 
+def regularize_times(reference, karaoke):
+  first_time = eval(reference[0][1])
+  for note_row in reference:
+    note_row[1] = eval(note_row[1]) - first_time
+    note_row[2] = eval(note_row[2]) - first_time
+  first_time = eval(karaoke[0][1])
+  for note_row in karaoke:
+    note_row[1] = eval(note_row[1]) - first_time
+    note_row[2] = eval(note_row[2]) - first_time
+  return reference, karaoke
+
+
+def notes_to_ints(reference, karaoke):
+  for i in range(len(reference)):
+    reference[i] = [eval(num) for num in reference[i]]
+  for i in range(len(karaoke)):
+    karaoke[i] = [eval(num) for num in karaoke[i]]
+  return reference, karaoke
+
+
 '''
   Note format is as follows:
-  [hz, time started, time ended]
+  [midi, time started, time ended]
 '''
 def get_notes(filename):
   notes = subprocess.check_output(["aubionotes", "-i", filename])
@@ -54,14 +69,26 @@ def get_notes(filename):
     No attention is paid to timing, nor magnitude of difference
 '''
 def grade(reference, karaoke):
+  # reference, karaoke = regularize_times(reference, karaoke)
+  # for row in zip(reference[0:50], karaoke[0:50]):
+  #   row = [row[0][0], row[1][0], str(row[0][1]), str(row[1][1]), str(row[0][2]), str(row[1][2])]
+  #   print " ".join(note for note in row)
+
   error_count = 0
-  for i in range(min(len(reference), len(karaoke))):
-    if i == len(reference) or i == len(karaoke):
-      error_count += max(len(reference), len(karaoke)) - i + 1
-    else:
-      if reference[i][0] != karaoke[i][0]:
-        error_count += 1
-  return error_count
+  error_amount = 0
+  reference, karaoke = notes_to_ints(reference, karaoke)
+
+  karaoke_index = 0
+  for i in range(len(reference) - 1):
+    while karaoke_index < len(karaoke) and \
+          (abs(karaoke[karaoke_index][1] - reference[i][1]) <= \
+          abs(karaoke[karaoke_index][1] - reference[i+1][1]) or \
+          abs(karaoke[karaoke_index][0] - reference[i][0]) <= \
+          abs(karaoke[karaoke_index][0] - reference[i+1][0])):
+      error_amount += abs(karaoke[karaoke_index][0] - reference[i][0])
+      karaoke_index += 1
+
+  print error_amount / karaoke_index
 
 
 def main():
@@ -78,10 +105,8 @@ def main():
   # karaoke_name = sys.argv[2] # Gotta figure out how this is stored
 
   ref_notes = get_notes(reference_name)
-  karaoke_notes = get_notes(karaoke_name)
+  karaoke_notes = get_notes(karaoke_name
 
-  print len(ref_notes)
-  print len(karaoke_notes)
   print grade(ref_notes, karaoke_notes)
 
 
